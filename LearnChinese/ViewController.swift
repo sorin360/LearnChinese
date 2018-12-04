3ee6eec4a1aa508555cb7e9484ed6d58f59200cc
Sorin Lica <sorinlica@sorins-mac.local>
2018-12-03 20:04:51 -0800

    Initial Commit

---
+++ LearnChinese/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnChinese
+//
+//  Created by Sorin Lica on 03/12/2018.
+//  Copyright © 2018 Sorin Lica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
